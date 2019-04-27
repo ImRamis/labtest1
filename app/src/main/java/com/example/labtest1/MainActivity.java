40a75c890cf76d6ebf7fc36f7518203fcefe06ad
Muhammad Ramis <mr.iramis@hotmail.com>
2019-04-27 11:14:29 +0500

    t

--- app/src/main/java/com/example/labtest1/MainActivity.java
+++ app/src/main/java/com/example/labtest1/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.labtest1;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    EditText latitute;
+    EditText longitute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        latitute = findViewById(R.id.latitute);
+        longitute = findViewById(R.id.longitute);
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (!latitute.getText().equals("") && !longitute.getText().equals("")) {
+                    try {
+                        double x2 = Double.parseDouble(latitute.getText().toString());
+                        double y2 = Double.parseDouble(longitute.getText().toString());
+                        double x1 = location.getLatitude();
+                        double y1 = location.getLongitude();
+                        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                        ((TextView) findViewById(R.id.result)).setText("Distance between point is " + distance + "");
+                    } catch (Exception e) {
+                        ((TextView) findViewById(R.id.result)).setText("Fix Input");
+                    }
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission Missing", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2,
+                2, listener);
+
+
     }
+
 }
